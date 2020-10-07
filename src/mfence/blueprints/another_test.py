2532efe3088dc037915b43c8cf3113a169201c5c
Marcelo Costa <marceloc@uchicago.edu>
2020-10-07 02:00:10 -0500

    add counters and gauges to sample endpoints

--- src/mfence/blueprints/another_test.py
+++ src/mfence/blueprints/another_test.py
@@ -1,7 +1,23 @@
 import flask
+from prometheus_client import Gauge
+from prometheus_client import Counter
+import time
+import random
+
+c = Counter('my_failures', 'Description of counter')
+g = Gauge('another_test_time', 'gauge example')
 
 blueprint = flask.Blueprint("another_test", __name__)
 
 @blueprint.route("/", methods=["GET", "POST"])
+#@g.track_inprogress()
+@c.count_exceptions()
 def another_test_page():
+    g.inc(21)
+    try:
+      time.sleep(random.randint(1,3))
+      if (random.randint(0,3) == 3):
+        raise Exception("illustrating exception counter")
+    finally:
+      g.dec(21)
     return "<h1>Another test!</h1>"
--- src/mfence/blueprints/test.py
+++ src/mfence/blueprints/test.py
@@ -1,9 +1,17 @@
 import flask
+from flask import request
+
+from prometheus_client import Counter
 
 blueprint = flask.Blueprint("test", __name__)
 
+c = Counter('test_info_requests', 'test_info requests', ['client_ip', 'endpoint'])
+# _total suffix is added automagically
+# two labels
+
 @blueprint.route("/", methods=["GET", "POST"])
 def test_info():
+    c.labels(request.remote_addr, 'test').inc()
     info = {
       "dummy_data": "foo",
       "whatever": 123
