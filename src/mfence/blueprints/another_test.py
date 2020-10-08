08fef06fd1afa9b110b61147f930eb1ac7ced7eb
Marcelo Costa <marceloc@uchicago.edu>
2020-10-08 15:43:08 -0500

    use track_inprogress() decorator

--- src/mfence/blueprints/another_test.py
+++ src/mfence/blueprints/another_test.py
@@ -10,14 +10,15 @@ g = Gauge('another_test_time', 'gauge example')
 blueprint = flask.Blueprint("another_test", __name__)
 
 @blueprint.route("/", methods=["GET", "POST"])
-#@g.track_inprogress()
+@g.track_inprogress()
 @c.count_exceptions()
 def another_test_page():
-    g.inc(21)
+    #g.inc(100)
     try:
-      time.sleep(random.randint(1,3))
+      time.sleep(random.randint(5,7))
       if (random.randint(0,3) == 3):
         raise Exception("illustrating exception counter")
     finally:
-      g.dec(21)
+      print('Done')
+      #g.dec(90)
     return "<h1>Another test!</h1>"
