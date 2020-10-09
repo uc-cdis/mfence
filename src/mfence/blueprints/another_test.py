e4638b975d0deeb863b5fd755086c0a1cb4243f8
Marcelo Costa <marceloc@uchicago.edu>
2020-10-09 00:14:18 -0500

    adding panel to illustrate average latency query with a summary

--- src/mfence/blueprints/another_test.py
+++ src/mfence/blueprints/another_test.py
@@ -16,17 +16,18 @@ blueprint = flask.Blueprint("another_test", __name__)
 @c.count_exceptions()
 def another_test_page():
     google_svc_acct_patch.labels(3, 'scientist.svc.acct1@someuniversity.iam.gserviceaccount.com').inc()
-    google_svc_acct_patch.labels(1, 'scientist.svc.acct2@someuniversity.iam.gserviceaccount.com').inc()
     try:
       # before running usersync
-      num_users_per_project.labels('1000Genomes').inc(200)
-      num_users_per_project.labels('CMG-Broad-GRU').inc(50)
+      num_users_per_project.labels('1000Genomes').set(200)
+      num_users_per_project.labels('CMG-Broad-GRU').set(50)
 
       time.sleep(random.randint(5,7))
       if (random.randint(0,3) == 3):
+        google_svc_acct_patch.labels(1, 'scientist.svc.acct2@someuniversity.iam.gserviceaccount.com').inc()
         raise Exception("illustrating exception counter")
     finally:
       print('Done')
       # after usersync
       num_users_per_project.labels('1000Genomes').dec(20)
+      num_users_per_project.labels('CMG-Broad-GRU').inc(10)
     return "<h1>Another test!</h1>"
